5bd3a634ccd9cc86383feaf1d01988d504a583de
FiantsoHarena <fiantsorav@gmail.com>
2025-03-01 15:36:35 +0300

    refactor: remove reset-project script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  extends: ["universe", "universe/native"],
+  extends: ['universe', 'universe/native'],
   rules: {
-    "import/order": 0,
-    "react-native/no-inline-styles": 0,
-    "import/namespace": 0,
-    "no-duplicate-imports": "error",
+    'import/order': 0,
+    'react-native/no-inline-styles': 0,
+    'import/namespace': 0,
+    'no-duplicate-imports': 'error',
   },
-};
+}
