7b313dd8a2146b969c6bee509fc893a2eb3af4ce
FiantsoHarena <fiantsorav@gmail.com>
2025-03-01 16:12:40 +0300

    chore: add prebuild:clean script

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import * as Styled from '@/styles/home.style';
 
 export default function Home() {
   return (
-    <Styled.Container style={{ fontFamily: 'Poppins-Bold', fontSize: 30 }}>
+    <Styled.Container>
       <Styled.Title>Hello, HEI Admin</Styled.Title>
     </Styled.Container>
   );
