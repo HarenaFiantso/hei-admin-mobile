9c71558039c6ef15fe68438a28140a091c809e9c
FiantsoHarena <fiantsorav@gmail.com>
2025-03-01 16:00:31 +0300

    style(font): poppins as font family

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { Text } from 'react-native'
 
 export default function Home() {
-  return <Text>Hello, HEI Admin</Text>
+  return <Text style={{ fontFamily: 'Poppins-Bold', fontSize: 30 }}>Hello, HEI Admin</Text>
 }
